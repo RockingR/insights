034da9b250d734d0a2f096dba8aa38db30795cef
RockingR <kennyliaocan@gmail>
2014-12-31 08:14:57 +0800

    add some suggestion when encountering errors

--- main.R
+++ main.R
@@ -7,3 +7,5 @@ options("getSymbols.warning4.0"=FALSE)
 options(shiny.trace=FALSE)
 
 runApp()
+
+# if some packages is not found, e.g. 'digest', then go to packages window to search and download 'digest' package. then re-run this file, then everything should be fine.
